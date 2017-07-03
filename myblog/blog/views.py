322cb4f90a2a580cedb2132583feec982b4fb1b6
Nitish shah <nitish800@users.noreply.github.com>
2017-07-03 17:05:40 +0530

    vieew updated

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import BlogPost,Categories
+
 
 # Create your views here.
+
+def index(request):
+    post_title_list = BlogPost.objects.order_by('-post_date')[:5]
+    context = {'post_title_list':post_title_list}
+    return render(request,'index.html',context)
+    
+
+def post(request):
+    return
+        
